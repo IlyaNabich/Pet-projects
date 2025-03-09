4754cb55f5b401f78d0db8cfde81d5163811675b
Ilya <ilnaiba@mail.ru>
2025-03-09 23:44:16 +0300

    Тест вывода моковых данных и их исправления

--- WebApplication3/Mocks/MockCars.cs
+++ WebApplication3/Mocks/MockCars.cs
@@ -91,46 +91,6 @@ namespace WebApplication3.Mocks
                         Avaliable = true,
                         Category = _CategoryCars.AllCategories.First()
                     },
-                    new Car {
-                        Name = "Nissan",
-                        ShortDesc = "",
-                        LongDesc = "",
-                        Img = "",
-                        Price = 60000,
-                        IsFavourite = true,
-                        Avaliable = true,
-                        Category = _CategoryCars.AllCategories.First()
-                    },
-                    new Car {
-                        Name = "Nissan",
-                        ShortDesc = "",
-                        LongDesc = "",
-                        Img = "",
-                        Price = 60000,
-                        IsFavourite = true,
-                        Avaliable = true,
-                        Category = _CategoryCars.AllCategories.First()
-                    },
-                    new Car {
-                        Name = "Nissan",
-                        ShortDesc = "",
-                        LongDesc = "",
-                        Img = "",
-                        Price = 60000,
-                        IsFavourite = true,
-                        Avaliable = true,
-                        Category = _CategoryCars.AllCategories.First()
-                    },
-                    new Car {
-                        Name = "Nissan",
-                        ShortDesc = "",
-                        LongDesc = "",
-                        Img = "",
-                        Price = 60000,
-                        IsFavourite = true,
-                        Avaliable = true,
-                        Category = _CategoryCars.AllCategories.First()
-                    },
                 };
             }
         }
